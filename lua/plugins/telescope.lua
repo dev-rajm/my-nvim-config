cf16b39de6404fdbd00a0979d4e2d7c68c3c44ab
rajmanna <dev.rajm8923@gmail.com>
2025-10-04 14:41:10 +0530

    added telescope-git-branch

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,29 +1,36 @@
-
 return {
-  {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 
-      'nvim-lua/plenary.nvim',
-      'duane9/nvim-rg'
-  },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      })
-      require("telescope").load_extension("ui-select")
-    end
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"duane9/nvim-rg",
+		},
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+	{
+		"mrloop/telescope-git-branch.nvim",
+		config = function()
+			require("telescope").load_extension("git_branch")
+			local git_branch = require("git_branch")
+			vim.keymap.set("n", "<leader>gb", git_branch.files, {})
+		end,
+	},
 }
