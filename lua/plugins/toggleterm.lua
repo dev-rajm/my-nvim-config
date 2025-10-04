8878ce4af75ebfb5e408d19943d0ff0c2346f86f
rajmanna <dev.rajm8923@gmail.com>
2025-10-04 20:00:27 +0530

    Updated output display for cpp

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,34 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	config = function()
+		require("toggleterm").setup({
+			direction = "float",
+			shade_terminals = true,
+		})
+
+		vim.keymap.set("n", "<C-B>", function()
+			local filename = vim.fn.expand("%:p")
+			local root = "D:/CP"
+			local exe = root .. "/a.exe"
+			local cmd = string.format(
+				'g++ -std=c++17 -O2 -Wall "%s" -o "%s" && "%s" < "%s/input.txt" > "%s/output.txt" && type "%s/output.txt"',
+				filename,
+				exe,
+				exe,
+				root,
+				root,
+				root,
+				root
+			)
+
+			require("toggleterm.terminal").Terminal
+				:new({ cmd = cmd, close_on_exit = false, direction = "float" })
+				:toggle()
+		end, { noremap = true, silent = true })
+
+		vim.keymap.set("n", "<C-o>", function()
+			vim.cmd("edit D:/CP/output.txt")
+		end, { noremap = true, silent = true })
+	end,
+}
--- luasnippets/cpp.lua
+++ luasnippets/cpp.lua
@@ -16,6 +16,12 @@ return {
       " ios::sync_with_stdio(false);",
       " cin.tie(0);",
       "",
+      "int tc;",
+      "cin >> tc;",
+      "while(tc--) {",
+      " solve();",
+      "}",
+      "",
       " return 0;",
       "}"
     }),
