26905645e3ef24cbe8f4c2dd08ea9d7f941cdb48
rajmanna <dev.rajm8923@gmail.com>
2025-10-07 20:46:15 +0530

    updated lspconfig

---
+++ lua/lsps/ts_ls.lua
@@ -0,0 +1,13 @@
+return {
+	cmd = { "bunx", "--bun", "typescript-language-server", "--stdio" },
+	filetypes = {
+		"vue",
+		"react",
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+}
