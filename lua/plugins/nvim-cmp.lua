4d51baadc4879e03a8d05dedb7d60c023a9d6de9
rajmanna <dev.rajm8923@gmail.com>
2025-10-07 20:25:31 +0530

    updated nvim-cmp

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,50 @@
+return {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup()
+		end,
+	},
+
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+				keyword_pattern = [[\k\+]],
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["C-y"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "pylsp" },
+				{ name = "cland" },
+				{ name = "rust_analyzer" },
+			}, {
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
+	end,
+}
