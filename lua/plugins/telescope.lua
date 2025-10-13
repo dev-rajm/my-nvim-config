ae0859ea501fe7a51f3c76c9a213467a704f734b
rajmanna <dev.rajm8923@gmail.com>
2025-10-13 11:49:51 +0530

    Update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,11 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"duane9/nvim-rg",
-		},
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader><leader>", builtin.help_tags, { desc = "Telescope help tags" })
 		end,
 	},
 	{
@@ -30,7 +26,7 @@ return {
 		config = function()
 			require("telescope").load_extension("git_branch")
 			local git_branch = require("git_branch")
-			vim.keymap.set("n", "<leader>gb", git_branch.files, {})
+			vim.keymap.set("n", "<leader>gb", git_branch.files, { desc = "Show git branch file differ preview " })
 		end,
 	},
 }
