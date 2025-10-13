13a28d698ba95cc881ffa4fd938378408a8172d1
rajmanna <dev.rajm8923@gmail.com>
2025-10-13 13:12:22 +0530

    Added assistant nvim

---
+++ lua/plugins/assistant-nvim.lua
@@ -0,0 +1,43 @@
+return {
+	"A7lavinraj/assistant.nvim",
+	lazy = false,
+	keys = {
+		{ "<leader>cpp", "<cmd>Assistant<cr>", desc = "Assistant.nvim" },
+	},
+	opts = {},
+	config = function()
+		require("assistant").setup({
+			mappings = {},
+			commands = {
+				cpp = {
+					extension = "cpp",
+					template = nil,
+					compile = {
+						main = "g++-15",
+						args = {
+							"$FILENAME_WITH_EXTENSION",
+							"-o",
+							"$FILENAME_WITHOUT_EXTENSION",
+							"-I/Users/jatinraghav/.local/include",
+							"-std=c++20",
+						},
+					},
+					execute = {
+						main = "./$FILENAME_WITHOUT_EXTENSION",
+						args = nil,
+					},
+				},
+			},
+			ui = {
+				border = "double",
+				diff_mode = true,
+				title_components_separator = ">",
+			},
+			core = {
+				process_budget = 5000,
+				port = 10043,
+				filename_generator = nil,
+			},
+		})
+	end,
+}
