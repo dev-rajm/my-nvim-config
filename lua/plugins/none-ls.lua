b3396cbd9bccbfb488616511ddb5c0db39bbfa0c
rajmanna <dev.rajm8923@gmail.com>
2025-10-26 12:22:57 +0530

    eslint to eslint_d

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -13,7 +13,7 @@ return {
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
         null_ls.builtins.completion.spell,
-        require("none-ls.diagnostics.eslint"),
+        require("none-ls.diagnostics.eslint_d"),
 			},
 		})
 
